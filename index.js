19ae9de374b9c6be90ea114f3581f0452e89f7e2
Hououin Kyouma <95170788+Kyouma960@users.noreply.github.com>
2022-05-01 03:37:01 +0000

    changed a lot of stuffs

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const keepAlive = require("./server");
 
 
 global.client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ],
-    partials: [
-        'CHANNEL', // Required to receive DMs
-    ],
-    disableMentions: 'everyone',
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES
+  ],
+  partials: [
+    'CHANNEL', // Required to receive DMs
+  ],
+  disableMentions: 'everyone',
 });
 
 client.config = require('./values/config');
